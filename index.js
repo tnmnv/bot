a5c95ebb112e98b0589ff663d17b1f33c39c1b5f
Nika <tanny.spynu@gmail.com>
2023-02-03 18:07:58 +0200

    new err corect

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const TelegramApi = require('node-telegram-bot-api')
 const {gameOptions, againOptions} = require('./options')
 const sequelize = require('./db')
+const UserModel = require('./models')
 
 const token = "5485612344:AAEmHkgh_W9NUdGbGjAktsv_Fd7zZivmpjM";
 
@@ -41,18 +42,28 @@ const start = async () => {
     bot.on('message', async msg=>{
         const text = msg.text;
         const chatId = msg.chat.id;
-    
-        if(text === '/start'){
-           await bot.sendSticker(chatId, 'https://tlgrm.eu/_/stickers/ea5/382/ea53826d-c192-376a-b766-e5abc535f1c9/7.webp')
-           return bot.sendMessage(chatId, `Dobro pojalovati`) 
-        }
-        if(text === '/info'){
-           return bot.sendMessage(chatId, `Tebea zovut ${msg.from.first_name}  ${msg.from.last_name}`) 
-        }
 
-        if(text === '/game'){
-            return startGame(chatId);
+        try {
+
+            if(text === '/start'){
+                await UserModel.create(chatId)
+                await bot.sendSticker(chatId, 'https://tlgrm.eu/_/stickers/ea5/382/ea53826d-c192-376a-b766-e5abc535f1c9/7.webp')
+                return bot.sendMessage(chatId, `Dobro pojalovati`) 
+             }
+             if(text === '/info'){
+                const user = await UserModel.findOne({chatId})
+                return bot.sendMessage(chatId, `Tebea zovut ${msg.from.first_name}  ${msg.from.last_name},  v igre u tebia pravelinih otvetov ${user.right}, nepravelinih otvetov ${user.wrong}`) 
+             }
+     
+             if(text === '/game'){
+                 return startGame(chatId);
+             }
+            
+        } catch (e) {
+            return bot.sendMessage(chatId, 'Proizoshla kakayata oshibka!')
         }
+    
+      
 
         return bot.sendMessage(chatId, "Ya ne ponimaiu tebya, poprobui esheo raz!");
     })
@@ -64,15 +75,22 @@ const start = async () => {
             if(data === '/again'){
                 return startGame(chatId);
             }
+
+            const user = await UserModel.findOne({chatId})
            
             if (data == chats[chatId]){
-             return bot.sendMessage(chatId, `Pozdrovlyaiu, ti otgdala tsifru ${chats[chatId]}`, againOptions)
+                user.right +=1;
+             await bot.sendMessage(chatId, `Pozdrovlyaiu, ti otgdala tsifru ${chats[chatId]}`, againOptions)
             }
             else{
-                return bot.sendMessage(chatId, `K sojaleniu ti ne ugadal, bot zagadal tsifru ${chats[chatId]}`, againOptions)
+                user.wrong +=1;
+                await bot.sendMessage(chatId, `K sojaleniu ti ne ugadal, bot zagadal tsifru ${chats[chatId]}`, againOptions)
             }
+
+            
             bot.sendMessage(chatId, `Ti vibral tsifru ${data}`)
             console.log(msg)
+            await user.save();
         })
        
     }
