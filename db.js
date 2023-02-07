361379f53d7f3bcf72ae60757ed1b9bdf0073e30
Nika <tanny.spynu@gmail.com>
2023-02-07 17:30:58 +0200

    adding another host

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ module.exports = new Sequelize(
     'imkrnqhodxhflb',
     'f35751f8f4a6b7ba44ea96e8aed84d62cc20eeb2525d86b99771051a478c4816',
     {
-        host:'ec2-63-32-248-14.eu-west-1.compute.amazonaws.com',
+        host:'postgres://imkrnqhodxhflb:f35751f8f4a6b7ba44ea96e8aed84d62cc20eeb2525d86b99771051a478c4816@ec2-63-32-248-14.eu-west-1.compute.amazonaws.com:5432/d3b55prf8boft5',
         port: '5432',
         dialect: 'postgres'
     }
